9f106c4a8018845d3fd4e1acb393669314c2e18d
André Perotto <andre@leadfy.me>
2024-09-25 18:59:25 +0000

    Publish 6 from andre into origin/main

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -7,9 +7,10 @@ import * as $$$$$$$$$1 from "./actions/createCompleteAirtableRecord.ts";
 import * as $$$$$$$$$$$0 from "./apps/deco/htmx.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 import * as $$$$$$0 from "./sections/FAQ.tsx";
+import * as $$$$$$10 from "./sections/features-leadfy.tsx";
 import * as $$$$$$1 from "./sections/Footer.tsx";
 import * as $$$$$$2 from "./sections/Header.tsx";
-import * as $$$$$$10 from "./sections/hero-leadfy.tsx";
+import * as $$$$$$11 from "./sections/hero-leadfy.tsx";
 import * as $$$$$$3 from "./sections/HeroLeadCapture.tsx";
 import * as $$$$$$4 from "./sections/ItWorks.tsx";
 import * as $$$$$$5 from "./sections/LeadCapture.tsx";
@@ -21,9 +22,10 @@ import * as $$$$$$9 from "./sections/Theme/Theme.tsx";
 const manifest = {
   "sections": {
     "site/sections/FAQ.tsx": $$$$$$0,
+    "site/sections/features-leadfy.tsx": $$$$$$10,
     "site/sections/Footer.tsx": $$$$$$1,
     "site/sections/Header.tsx": $$$$$$2,
-    "site/sections/hero-leadfy.tsx": $$$$$$10,
+    "site/sections/hero-leadfy.tsx": $$$$$$11,
     "site/sections/HeroLeadCapture.tsx": $$$$$$3,
     "site/sections/ItWorks.tsx": $$$$$$4,
     "site/sections/LeadCapture.tsx": $$$$$$5,
